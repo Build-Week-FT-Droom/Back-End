d53958a5ee590755429948e163ec5665bdbaf532
Steve Renner <che1111@hotmail.com>
2019-11-16 14:38:24 -0800

    added auth put routes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -69,3 +69,23 @@ router.post("/login", (req, res) => {
     }
 });
 
+// AUTH PUT ROUTES
+
+router.put('/user/:id', validateToken, (req, res) => {
+    const changes = req.body;
+    db.update(req.params.id, changes)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: 'Invalid user' });
+            }
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ message: 'Could not update user'}, error);
+        });
+});
+
+
+
