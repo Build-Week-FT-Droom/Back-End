ae189ec1fac3ad290236fd2191de8e56f27ff066
Steve Renner <che1111@hotmail.com>
2019-11-16 14:34:19 -0800

    Added auth post routes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -27,3 +27,45 @@ router.get('/user/:id', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+// AUTH POST ROUTES
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10)
+
+    user.password = hash;
+
+    db.add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+    let { email, password } = req.body;
+
+    if (email && password) {
+        db.findBy({ email })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    const token = generateToken(user);
+                    res.status(200).json({ message: `Welcome ${user.first_name}`, token, userId: user.id })
+                } else {
+                    res.status(401).json({ message: "Please enter a valid email and password" });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ message: `server error ${error}` });
+            });
+    } else {
+        res.status(400).json({ message: `Username and password required` });
+    }
+});
+
