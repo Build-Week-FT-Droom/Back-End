89058488536ce217bbfc14cd603b0d98b9f29723
Steve Renner <che1111@hotmail.com>
2019-11-16 17:22:08 -0800

    updated knex file

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -12,23 +12,22 @@ exports.seed = function(knex) {
           "interests": "Long walks on the beach"
         },
         { 
-        "first_name": "Jane",
-        "last_name": "Doe",
-        "email": "janedoe@hotmail.com",
-        "password": "E@kd92",
-        "occupation": "President",
-        "past_experience": "Vice President",
-        "interests": "Making people work hard"
+          "first_name": "Jane",
+          "last_name": "Doe",
+          "email": "janedoe@hotmail.com",
+          "password": "E@kd92",
+          "occupation": "President",
+          "past_experience": "Vice President",
+          "interests": "Making people work hard"
         },
         {
-        "first_name": "Skippy",
-        "last_name": "McGee",
-        "email": "skipster@msn.com",
-        "password": "E@kd92",
-        "occupation": "Beatnik",
-        "past_experience": "Saving the world",
-        "interests": "Inclusivity"
+          "first_name": "Skippy",
+          "last_name": "McGee",
+          "email": "skipster@msn.com",
+          "password": "E@kd92",
+          "occupation": "Beatnik",
+          "past_experience": "Saving the world",
+          "interests": "Inclusivity"
         }
       ]);
-    
 };
