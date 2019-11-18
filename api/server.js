c4b5324a8a464bed47513aed0cdd5366e63c0b45
Steve Renner <che1111@hotmail.com>
2019-11-17 17:50:32 -0800

    updated server.js to add jobs router

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const authRouter = require('../auth/auth-router.js');
+const jobsRouter = require('../jobs/jobs-router.js');
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
+server.use('/api/', jobsRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json(`Droom Sanity Check`);
