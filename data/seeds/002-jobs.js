da292a1bb5e644872deeddf5d2147402959a381c
Steve Renner <che1111@hotmail.com>
2019-11-19 10:37:29 -0800

    updated seeds

--- data/seeds/002-jobs.js
+++ data/seeds/002-jobs.js
@@ -23,49 +23,49 @@ exports.seed = function(knex) {
         "description": "Craft and fine artists typically do the following: Use techniques, such as knitting, weaving, glass blowing, painting, drawing, or sculpting."
       },
       {
-        "user_id": 4,
+        "user_id": 1,
         "img_url": 'https://ibb.co/j8ThJBR.jpg',
         "title": "Detective",
         "salary": "$90,000 per year",
         "description": "Responsible for investigating crimes such as burglaries, homicides, assaults, and other incidents to find and implicate perpetrators. Collects evidence, interviews witnesses, and testifies in court."
       },
       {
-        "user_id": 5,
+        "user_id": 2,
         "img_url": 'https://ibb.co/cgYKzhj.jpg',
         "title": "Voice Actor",
         "salary": "$55,000 per year",
         "description": "Perform in many roles, including providing narratives in commercials or playing characters in animated films or video games."
       },
       {
-        "user_id": 6,
+        "user_id": 3,
         "img_url": 'https://ibb.co/nrSVdNY.jpg',
         "title": "Chocolate Taster",
         "salary": "$57,000 per year",
         "description": "You will work alongside approximately 11 Chocolate Tasters and a panel leader; sharing opinions and collaborating with others to reach an agreement on taste."
       },
       {
-        "user_id": 7,
+        "user_id": 1,
         "img_url": 'https://ibb.co/nfgMMSb.jpg',
         "title": "Mystery Shopper",
         "salary": "$52,000 per year",
         "description": "Pose as regular customers in order to gauge and report on the quality of products, customer service, and environment of the stores to which you are assigned."
       },
       {
-        "user_id": 8,
+        "user_id": 2,
         "img_url": 'https://ibb.co/RYG3tcZ.jpg',
         "title": "Interior Designer",
         "salary": "$73,000 per year",
         "description": "Make interior spaces functional, safe, and beautiful by determining space requirements and selecting decorative items, such as colors, lighting, and materials."
       },
       {
-        "user_id": 9,
+        "user_id": 3,
         "img_url": 'https://ibb.co/C7Qmnqj.jpg',
         "title": "Brew Master",
         "salary": "$68,000 per year",
         "description": "Use your passion for beer and comprehensive knowledge of brewing processes to craft creative recipes and produce the best, most exciting beers possible."
       },
       {
-        "user_id": 10,
+        "user_id": 1,
         "img_url": 'https://ibb.co/pjvBBhY.jpg',
         "title": "Astronaut",
         "salary": "$170,000 per year",
