b71133d8e03824149dd9750926605d4ec083da59
Steve Renner <che1111@hotmail.com>
2019-11-20 14:22:50 -0800

    updated img_url

--- data/seeds/002-jobs.js
+++ data/seeds/002-jobs.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
   return knex('jobs').insert([
       {
         "user_id": 1,
-        "img_url": 'https://unsplash.com/photos/evlkOfkQ5rE',
+        "img_url": 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         "title": "Movie Critic",
         "salary": "$75,000 per year",
         "description": "As part of their job, a movie critic may evaluate several movies a week, report on general entertainment news, attend film festivals and premieres and analyze trends in movie ticket sales. Movie critics discuss their opinions on films in broadcast media"
