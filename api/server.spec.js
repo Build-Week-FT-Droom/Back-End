32f80b9df4f26f475a3693fc9a71e234a2ab9baf
Steve Renner <che1111@hotmail.com>
2019-11-20 10:01:28 -0800

    added testing

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,29 +5,24 @@ const server = require('./server.js');
 describe('server', function() {
     describe('GET /', function() {
         it('should return 200 OK', function() {
-            // run the server
-
-            // make a GET request to /
+        
             return request(server)
             .get('/')
             .then(res => {
                 expect(res.status).toBe(200);
             });
-            // see that the http code of response is 200
+           
 
 
         });
         
         it('should return JSON formatted response', function() {
-            // run the server
-
-            // make a GET request to /
+            
             return request(server)
             .get('/')
             .then(res => {
                 expect(res.type).toMatch(/json/i);
             });
-            // see that the http code of response is 200
 
 
         });
